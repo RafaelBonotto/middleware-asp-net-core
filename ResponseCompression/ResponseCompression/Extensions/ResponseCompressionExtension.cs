f153711bd31ba04fcfcd20b06191ce20e7dd7fc2
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-10-21 21:33:21 -0300

    Update ResponseCompressionExtension.cs

--- ResponseCompression/ResponseCompression/Extensions/ResponseCompressionExtension.cs
+++ ResponseCompression/ResponseCompression/Extensions/ResponseCompressionExtension.cs
@@ -1,10 +1,19 @@
-﻿namespace ResponseCompression.Extensions
+﻿using Microsoft.AspNetCore.ResponseCompression;
+
+namespace ResponseCompression.Extensions
 {
     public static class ResponseCompressionExtension
     {
-        public static void ConfigureResponseCompression(this IServiceCollection service)
+        public static void ConfigureResponseCompression(this IServiceCollection services)
         {
+            services.Configure<GzipCompressionProviderOptions>(options => 
+                options.Level = System.IO.Compression.CompressionLevel.Optimal);
 
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            });
         }
     }
 }
