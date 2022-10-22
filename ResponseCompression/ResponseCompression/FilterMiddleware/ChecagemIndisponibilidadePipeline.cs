40738e401ed9cde43dbe195f6e605b507720f372
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-10-22 01:14:19 -0300

    Middleware customizado 2 - Classe responsável por habilitar o middleware na action ou na controller especifica

---
+++ ResponseCompression/ResponseCompression/FilterMiddleware/ChecagemIndisponibilidadePipeline.cs
@@ -0,0 +1,10 @@
+﻿using ResponseCompression.Middleware;
+
+namespace ResponseCompression.FilterMiddleware
+{
+    public class ChecagemIndisponibilidadePipeline
+    {
+        public void Configure(IApplicationBuilder app)
+            => app.UseMiddleware<ChecagemIndisponibilidade>();
+    }
+}
